8606a0e4bec81e1220a1cfba6ced263f03a566bb
Jan Zimmek <jan.zimmek@web.de>
2017-03-17 20:00:01 +0100

    initial

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import express from "express"
+import chokidar from "chokidar"
+
+export default (app,{requireFile,watch,clearIf,watchOpts}) => {
+  let router = express.Router(),
+      {default: fn, tearDown} = require(requireFile)
+
+  fn(router)
+
+  chokidar.watch(watch, {
+    awaitWriteFinish: {
+      stabilityThreshold: 500,
+      pollInterval: 100,
+    },
+    ...watchOpts
+  }).on("all", () => {
+    if(tearDown)
+      tearDown()
+
+    Object.keys(require.cache).forEach(function(key){
+      if(clearIf(key))
+        delete require.cache[key]
+    })
+
+    let {default: nextFn, tearDown: nextTearDown} = require(requireFile)
+
+    router = express.Router()
+    nextFn(router)
+    tearDown = nextTearDown
+  })
+
+  app.use("/", (req,res,next) => {
+    router(req,res,next)
+  })
+}
